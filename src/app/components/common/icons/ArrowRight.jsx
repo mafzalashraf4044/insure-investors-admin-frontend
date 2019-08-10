3fe9758868e6f0a8f3d1908da9c556320a01724f
Afzal Ashraf <afzalashraf.dev@gmail.com>
2019-08-10 13:32:58 +0500

    overview page icons added

---
+++ src/app/components/common/icons/ArrowRight.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArrowRight = props => (
+  <svg width="1em" height="1em" viewBox="0 0 64 43.98" {...props}>
+    <path
+      d="M43.1.663a2.291 2.291 0 00-3.224 0 2.188 2.188 0 000 3.12l16.352 15.98H2.256a2.224 2.224 0 100 4.448h53.969L39.873 40.168a2.226 2.226 0 000 3.152 2.3 2.3 0 003.224 0l20.228-19.768a2.14 2.14 0 000-3.12z"
+      fill="#00a8d2"
+      fillRule="evenodd"
+    />
+  </svg>
+);
+
+export default ArrowRight;
---
+++ src/app/components/common/icons/Caret.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Caret = props => (
+  <svg width="1em" height="1em" viewBox="0 0 64 34.907" {...props}>
+    <path d="M.004 0l32 34.907L64.004 0z" fill="#223352" fillRule="evenodd" />
+  </svg>
+);
+
+export default Caret;
