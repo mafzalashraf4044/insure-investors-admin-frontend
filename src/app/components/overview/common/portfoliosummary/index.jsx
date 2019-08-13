4afe093a151fd44cd321763f2e8b5ba95e973106
Afzal Ashraf <afzalashraf.dev@gmail.com>
2019-08-13 08:27:41 +0500

    overview page portfolio summary basic complete

--- src/app/components/overview/common/portfoliosummary/index.jsx
+++ src/app/components/overview/common/portfoliosummary/index.jsx
@@ -3,13 +3,76 @@ import React from 'react';
 //  styled components
 import {
   PortfolioSummary,
-
+  Header,
+  Title,
+  AddPropertyButton,
+  AddIcon,
+  AddPropertyButtonText
 } from './styled';
+
+import DescriptionList from 'components/common/descriptionlist'; 
+import {
+  DescriptionListItem,
+  DescriptionListFields,
+  DescriptionListField, 
+  DescriptionTitle,
+  DescriptionValue
+} from 'components/common/descriptionlist/styled';
 
 export default () => {
 
+  const properties = [
+    {}, {}, {}, {}, {},
+  ];
+
+  const renderProperty = (o, i) => {
+    console.log('hello', i);
+    
+    return (
+      <DescriptionListItem isArray={false} key={i}>  
+        <DescriptionListFields>
+
+          <DescriptionListField>
+            <DescriptionTitle>Property Address</DescriptionTitle>
+            <DescriptionValue>3100 Brackenridge St,  Houston, TX, 77026</DescriptionValue>
+          </DescriptionListField>
+
+          <DescriptionListField>
+            <DescriptionTitle>Coverage Premiums</DescriptionTitle>
+            <DescriptionValue></DescriptionValue>
+          </DescriptionListField>
+
+          <DescriptionListField>
+            <DescriptionTitle>Final Premium</DescriptionTitle>
+            <DescriptionValue>$2,500</DescriptionValue>
+          </DescriptionListField>
+  
+        </DescriptionListFields>
+      </DescriptionListItem>
+    );
+  };
+
+
   return (
     <PortfolioSummary>
+      <Header>
+        <Title>Portfolio Summary</Title>
+
+        <AddPropertyButton>
+          <AddIcon />
+          <AddPropertyButtonText>Add Property</AddPropertyButtonText>
+        </AddPropertyButton>
+      </Header>
+
+      {
+        properties.map((property, i) => (
+          <DescriptionList
+            key={i}
+            data={property}
+            renderListItem={renderProperty}
+          />
+        ))
+      }
 
     </PortfolioSummary>
   );
